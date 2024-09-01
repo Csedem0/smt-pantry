badbf7d0e6b2e60289dbdab78a0b4b56464059f7
Csedem0 <emmasobula@gmail.com>
2024-09-01 22:59:43 +0100

    helper

--- prediction/urls.py
+++ prediction/urls.py
@@ -17,12 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/accounts/', permanent=False)),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
-
-urlpatterns += staticfiles_urlpatterns()
