67bbf72d8665a3f7bf412aee2ed9c1299ff4c902
Csedem0 <emmasobula@gmail.com>
2024-09-01 23:12:33 +0100

    yes o

--- prediction/urls.py
+++ prediction/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
+
 urlpatterns = [
     path('', RedirectView.as_view(url='/accounts/', permanent=False)),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
+
